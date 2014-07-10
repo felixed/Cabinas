6e77b0307afcc9ec34d7dd61a0abd884c2ee1c04
Diego Rosales <diegorosales.25@gmail.com>
2014-07-10 13:15:38 -0500

    codigo de prueba
    codigo de prueba

--- Código/MP3 Prueba/Prueba/Prueba.ino
+++ Código/MP3 Prueba/Prueba/Prueba.ino
@@ -6,10 +6,10 @@
 
 #include <Wtv020sd16p.h>
 
-int resetPin = 2;  // The pin number of the reset pin.
-int clockPin = 3;  // The pin number of the clock pin.
-int dataPin = 4;  // The pin number of the data pin.
-int busyPin = 5;  // The pin number of the busy pin.
+int resetPin = 13;//2;  // The pin number of the reset pin.
+int clockPin = A2;//3;  // The pin number of the clock pin.
+int dataPin = A3;//4;  // The pin number of the data pin.
+int busyPin = 11;//5;  // The pin number of the busy pin.
 
 /*
 Create an instance of the Wtv020sd16p class.
@@ -21,19 +21,45 @@ Create an instance of the Wtv020sd16p class.
 Wtv020sd16p wtv020sd16p(resetPin,clockPin,dataPin,busyPin);
 int x = 0;
 void setup() {
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(busyPin, INPUT);
+  pinMode(resetPin, OUTPUT);
+  
+  digitalWrite(clockPin, HIGH);
+  digitalWrite(dataPin, HIGH);
+  digitalWrite(resetPin, HIGH);
+  //----
+  delay(1000);
+  digitalWrite(clockPin, LOW);
+  digitalWrite(dataPin, LOW);
+  digitalWrite(resetPin, LOW);
+  
+  
+  
+  Serial.begin(9600);
   //Initializes the module.
+  //delay(1000);
   wtv020sd16p.reset();
   //Plays synchronously an audio file. Busy pin is used for this method.
-  wtv020sd16p.playVoice(0);
+  wtv020sd16p.asyncPlayVoice(0);
 }
 
 void loop() {
   
-  if(Serial.available()){
+  /*if(Serial.available()){
     x = Serial.parseInt();
-    wtv020sd16p.playVoice(x);
-    Serial.println(x);
-  }
+    //Serial.println(x);
+    wtv020sd16p.pauseVoice();
+    wtv020sd16p.asyncPlayVoice(x);
+    //Serial.println(x);
+    //delay(10000);    
+  }*/
+  
+  delay(10000);
+  wtv020sd16p.asyncPlayVoice(0);
+ 
+  
 
 }
 
