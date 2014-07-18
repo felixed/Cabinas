8d916b6502fe9997b33de532cee6db70c9244763
Diego Rosales <diegorosales.25@gmail.com>
2014-07-18 13:15:20 -0500

    Botonera Panel
    Botonera Panel

--- Código/Master_i2c/Keypad_I2C/Keypad_I2C.ino
+++ Código/Master_i2c/Keypad_I2C/Keypad_I2C.ino
@@ -22,9 +22,9 @@ char keys[ROWS][COLS] = {
   {'#','0','*','D'}
 };
 // Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-byte rowPins[ROWS] = { 5, 6, 7, 8 };
+byte rowPins[ROWS] = { 2, 3, 4, 5 }; 
 // Connect keypad COL0, COL1 and COL2 to these Arduino pins.
-byte colPins[COLS] = { 9, 10, 11, 12 }; 
+byte colPins[COLS] = { 6, 7, 8, 9 };
 
 // Create the Keypad
 Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
